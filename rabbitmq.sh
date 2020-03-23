89acd8a7b7e8ec04c47f96b816332f762a51f39e
myucel <myucel@harezmi.com.tr>
2020-03-23 13:58:02 +0300

    added kubernates deployment support and rabbitmq metrics

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,2 +1,4 @@
-docker run -p 5672:5672 -p 15672:15672 --hostname localhost -d --name rabbit rabbitmq:3-management
+docker run -p 5672:5672 -p 15672:15672 -p 15692:15692 --hostname localhost -d --name rabbit rabbitmq:3-management
 docker exec -it rabbit rabbitmq-plugins enable rabbitmq_consistent_hash_exchange
+docker exec -it rabbit rabbitmq-plugins enable rabbitmq_prometheus
+
